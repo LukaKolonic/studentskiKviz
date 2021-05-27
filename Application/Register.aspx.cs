6fe972b041517a0663e5e750e873a9331522395f
Mancek <52491015+Mancek@users.noreply.github.com>
2021-05-27 20:22:24 +0200

    Register form & DAL

---
+++ Application/Register.aspx.cs
@@ -0,0 +1,32 @@
+﻿using Application.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Application
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnitOfWork dataSource = new UnitOfWork();
+
+            /*unitofWork.Users.Insert(new User
+            {
+                Ime = "Manuel",
+                Prezime = "Plasc",
+                Email = "mancek78@gmail.com",
+                Password = BC.HashPassword("mancek123")
+            });
+            unitofWork.Commit();*/
+
+            string email = "mancek789@gmail.com";
+            bool IsUnique = dataSource.Users.Get(u => u.Email == email).FirstOrDefault() == null;
+            //bool authenticated = BC.Verify("mancek12s3", user.Password);
+            test.Text = IsUnique.ToString();
+        }
+    }
+}
